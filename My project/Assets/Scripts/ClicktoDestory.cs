1f4ba75deb763f554b585b3e77f02e21049be34c
obaidah <obhureini@gmail.com>
2024-03-08 19:17:07 +0300

    test

--- My project/Assets/Scripts/ClicktoDestory.cs
+++ My project/Assets/Scripts/ClicktoDestory.cs
@@ -1,25 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
-
-public class ClicktoDestory : MonoBehaviour
+using UnityEngine.UI;
+public class ScoreTracker : MonoBehaviour
 {
-    private ScoreTracker scoreTracker; // Reference to the ScoreTracker script
+    public static int scoreValue = 0;
+    public Text scoreText;
 
-    private void Start()
+
+    // Start is called before the first frame update
+    void Start()
     {
-        // Find the ScoreTracker script in the scene
-        scoreTracker = FindObjectOfType<ScoreTracker>();
-        if (scoreTracker == null)
-        {
-            Debug.LogError("ScoreTracker script not found!");
-        }
+        
     }
 
-    private void OnMouseDown()
+    // Update is called once per frame
+    void Update()
     {
-        Destroy(gameObject);
-        if (scoreTracker != null)
-        {
-            scoreTracker.IncreaseScore(); // Call the IncreaseScore method from ScoreTracker
-        }
+
+        scoreText.text = "hello";
     }
+
 }
--- My project/Assets/Scripts/ScoreTracker.cs
+++ My project/Assets/Scripts/ScoreTracker.cs
@@ -1,45 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-
 public class ScoreTracker : MonoBehaviour
 {
-    private int score = 0; // Variable to store the score
-    private Text scoreText; // Reference to the UI Text component for displaying the score
+    public static int scoreValue = 0;
+    Text score;
 
-    private void Start()
+
+    // Start is called before the first frame update
+    void Start()
     {
-        // Find the UI Text component with a specific tag
-        GameObject textObject = GameObject.FindGameObjectWithTag("ScoreText");
-        if (textObject != null)
-        {
-            scoreText = textObject.GetComponent<Text>();
-            if (scoreText == null)
-            {
-                Debug.LogError("UI Text component not found on the object with the 'ScoreText' tag.");
-            }
-            else
-            {
-                UpdateScoreText(); // Update the score text when the game starts
-            }
-        }
-        else
-        {
-            Debug.LogError("No object found with the 'ScoreText' tag.");
-        }
+        score = GetComponent<Text>();
     }
 
-    public void IncreaseScore()
+    // Update is called once per frame
+    void Update()
     {
-        score++; // Increment the score
-        UpdateScoreText(); // Update the score text
+        score.text = "Score: " + scoreValue;
+
     }
 
-    private void UpdateScoreText()
-    {
-        // Update the text of the UI Text component to display the current score
-        if (scoreText != null)
-        {
-            scoreText.text = "Score: " + score.ToString();
-        }
-    }
 }
